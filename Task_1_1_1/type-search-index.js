af8053aec9d3169f9af4f73f21dbbc470e413902
olesik111 <olesik111@users.noreply.github.com>
2025-09-02 13:08:49 +0000

    Deploying to gh-pages from @ olesik111/OOP@023bef764afbaa774207b59f26c6637d573c8092 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kataeva","l":"Sample"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.kataeva","l":"HeapSort"}];updateSearchResults();
