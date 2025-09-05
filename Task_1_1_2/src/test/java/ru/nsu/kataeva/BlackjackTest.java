a55f8c9a4c3b319506d5574aeda03d06619e667d
olesik111 <o.kataeva@g.nsu.ru>
2025-09-06 00:04:44 +0700

    bj test add

--- Task_1_1_2/src/test/java/ru/nsu/kataeva/BlackjackTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kataeva/BlackjackTest.java
@@ -27,52 +27,13 @@ class BlackjackTest {
 
         assertDoesNotThrow(() -> {
             Blackjack game = new Blackjack();
-            // Проверяем, что колоды созданы
             assertNotNull(game);
         });
 
         System.setIn(System.in);
     }
 
-    @Test
-    void testDealInitialCards() throws Exception {
-        Blackjack game = new Blackjack();
-        Deck player = new Deck();
-        Deck dealer = new Deck();
-        Deck gameDeck = new Deck();
-        gameDeck.createDeck();
 
-        // Тестируем приватный метод через reflection
-        Method dealInitialCards = Blackjack.class.getDeclaredMethod("dealInitialCards", Deck.class, Deck.class);
-        dealInitialCards.setAccessible(true);
-        dealInitialCards.invoke(game, player, dealer);
-
-        assertEquals(2, player.toString().split(", ").length);
-        assertEquals(2, dealer.toString().split(", ").length);
-    }
-
-    @Test
-    void testShowInitialHands() throws Exception {
-        Blackjack game = new Blackjack();
-        Deck player = new Deck();
-        Deck dealer = new Deck();
-
-        player.addCardForTest(new Card(Suit.HEART, Value.ACE));
-        player.addCardForTest(new Card(Suit.SPADE, Value.KING));
-
-        dealer.addCardForTest(new Card(Suit.DIAMOND, Value.QUEEN));
-        dealer.addCardForTest(new Card(Suit.CLUB, Value.JACK));
-
-        // Тестируем приватный метод через reflection
-        Method showInitialHands = Blackjack.class.getDeclaredMethod("showInitialHands", Deck.class, Deck.class);
-        showInitialHands.setAccessible(true);
-        showInitialHands.invoke(game, player, dealer);
-
-        String output = outputStream.toString();
-        assertTrue(output.contains("Your hand:"));
-        assertTrue(output.contains("Dealer's hand:"));
-        assertTrue(output.contains("Hidden Card"));
-    }
 
     @Test
     void testGameWithImmediateQuit() {
