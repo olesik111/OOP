c6bee1ac190de349ac705ae2a6e7cddf5431f410
olesik111 <o.kataeva@g.nsu.ru>
2025-09-05 19:56:24 +0700

    Delete Task_1_1_1 directory
    не туда

--- Task_1_1_1/sborka.sh
+++
@@ -1,5 +0,0 @@
-javadoc -d build/docs/javadoc -sourcepath src/main/java -subpackages ru.nsu.kataeva
-
-javac src/main/java/ru/nsu/kataeva/HeapSort.java -d ./build
-
-java -cp ./build ru.nsu.kataeva.HeapSort
