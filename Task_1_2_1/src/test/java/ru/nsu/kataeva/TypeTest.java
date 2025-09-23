5495f4636427145fa08d5f0fd33eb9a658aa58b1
olesik111 <o.kataeva@g.nsu.ru>
2025-09-23 23:21:03 +0700

    test fix

--- Task_1_2_1/src/test/java/ru/nsu/kataeva/TypeTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/kataeva/TypeTest.java
@@ -1,6 +1,5 @@
 package ru.nsu.kataeva;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
@@ -17,22 +16,21 @@ public class TypeTest {
         graph1.addEdge("A", "B");
 
 
-        AdjacencyMatrix<Object> graph2 = new AdjacencyMatrix<>();
-        graph2.addVertex("A");
-        graph2.addVertex("B");
-        graph2.addEdge("A", "B");
+        AdjacencyMatrix<Integer> graph2 = new AdjacencyMatrix<>();
+        graph2.addVertex(1);
+        graph2.addVertex(2);
+        graph2.addEdge(1, 2);
 
-        assertEquals(graph1, graph2);
-        assertEquals(graph2, graph1);
+        assertNotEquals(graph1, graph2);
     }
 
     @Test
     void testNotEqualsDifferentVertices() {
         AdjacencyMatrix<String> graph1 = new AdjacencyMatrix<>();
-        graph1.addVertex("A");
+        graph1.addVertex("1");
 
-        AdjacencyMatrix<Object> graph2 = new AdjacencyMatrix<>();
-        graph2.addVertex("X");
+        AdjacencyMatrix<Integer> graph2 = new AdjacencyMatrix<>();
+        graph2.addVertex(1);
 
         assertNotEquals(graph1, graph2);
     }
