0edbadb32e7205950eda19bf7327fbf06ad70437
olesik111 <o.kataeva@g.nsu.ru>
2025-10-03 00:15:37 +0700

    reviewdog

--- Task_1_4_1/src/main/java/ru/nsu/kataeva/Subject.java
+++ Task_1_4_1/src/main/java/ru/nsu/kataeva/Subject.java
@@ -7,6 +7,7 @@ public class Subject {
     private final String name;
     private final int finalSemester;
     private final GradeType finalGradeType;
+
     /**
      * Constructor.
      *
@@ -15,7 +16,8 @@ public class Subject {
      */
     public Subject(String name, int finalSemester, GradeType finalGradeType) {
         if (finalSemester < 1) {
-            throw new IllegalArgumentException("Final semester must be >= 1, but was: " + finalSemester);
+            throw new IllegalArgumentException("Final semester must be >= 1, but was: "
+                    + finalSemester);
         }
         this.name = name;
         this.finalSemester = finalSemester;
